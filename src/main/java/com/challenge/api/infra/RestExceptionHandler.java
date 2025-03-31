262d2487f114f1a39bc61a66fcca896f2e200a6e
Fabrício <fazinho1999@hotmail.com>
2025-03-31 17:29:13 -0300

    CorrecaoErrorMessagePut

--- src/main/java/com/challenge/api/infra/RestExceptionHandler.java
+++ src/main/java/com/challenge/api/infra/RestExceptionHandler.java
@@ -4,6 +4,7 @@ import com.challenge.api.dto.GetConsentByIdResponseDTO;
 import com.challenge.api.exception.GetConsentByIdException;
 import com.challenge.api.exception.PostConsentsDtoException;
 import com.challenge.api.exception.PostConsentsException;
+import com.challenge.api.exception.PutConsentByIdException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,19 +16,25 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(PostConsentsException.class)
     private ResponseEntity<ConsentsErroMessage> invalidCpf(PostConsentsException exception){
-        ConsentsErroMessage treatResponse = new ConsentsErroMessage(HttpStatus.BAD_REQUEST,exception.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(treatResponse);
+        ConsentsErroMessage handlePostBadRequest = new ConsentsErroMessage(HttpStatus.BAD_REQUEST,exception.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(handlePostBadRequest);
     }
 
     @ExceptionHandler(PostConsentsDtoException.class)
     private ResponseEntity<ConsentsErroMessage> invalidType(PostConsentsDtoException exception){
-        ConsentsErroMessage treatRequestDto = new ConsentsErroMessage(HttpStatus.BAD_REQUEST, exception.getMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(treatRequestDto);
+        ConsentsErroMessage handlePostBadRequest = new ConsentsErroMessage(HttpStatus.BAD_REQUEST, exception.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(handlePostBadRequest);
     }
 
     @ExceptionHandler(GetConsentByIdException.class)
     private ResponseEntity<ConsentsErroMessage> consentNotFound(GetConsentByIdException exception){
-        ConsentsErroMessage treatGetConsentResponseDTO = new ConsentsErroMessage(HttpStatus.NOT_FOUND, exception.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(treatGetConsentResponseDTO);
+        ConsentsErroMessage handleGetConsentIdNotFound = new ConsentsErroMessage(HttpStatus.NOT_FOUND, exception.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(handleGetConsentIdNotFound);
+    }
+
+    @ExceptionHandler(PutConsentByIdException.class)
+    private ResponseEntity<ConsentsErroMessage> consentExpirationDateTimeInvalid(PutConsentByIdException exception){
+        ConsentsErroMessage handlePutExpirationDateTimeInvalid = new ConsentsErroMessage(HttpStatus.BAD_REQUEST, exception.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(handlePutExpirationDateTimeInvalid);
     }
 }
